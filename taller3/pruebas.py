790ddd15a46c908fba194a9223694f986f733a8d
LeidyAldana <LeidyMarcelaAldana@gmail.com>
2019-04-26 21:48:30 -0500

    Duplicación de bordes

--- taller3/pruebas.py
+++ taller3/pruebas.py
@@ -8,9 +8,24 @@ Created on Fri Apr 26 18:35:14 2019
 
 import numpy as np
 
-b = np.full((11,11),5)
+#b = np.full((3,3),5)
+b = np.random.random((3,3))
+print(b)
 #b= np.append(b, b[:,0], axis=0)
-b[:,0]=111
+array=[1,2,3]
+array2=[4,5,6,7,8]
+#b = np.insert(b, 0, b[:,0], axis=0)
+#b = np.resize(b,(b.shape[0]+1,b.shape[1]+1))
+
+# Duplicando valores para x
+b = np.insert(b, 0, b[0,:], axis=0)
+b = np.insert(b, b.shape[0], b[(b.shape[0]-1),:], axis=0)
+print(b)
+print("Forma de b: ")
+print(b.shape)
+# Duplicando valores para y
+b = np.insert(b, 0, b[:,0], axis=1)
+b = np.insert(b, b.shape[1], b[:,(b.shape[1]-1)], axis=1)
 print(b)
 mascara = np.full((3,3),1/5) 
 #print ("Slice: ",b.shape) 
@@ -18,11 +33,14 @@ mascara = np.full((3,3),1/5)
 #print(b)
 elem=0
 #print (mascara)
-for i in range(0, (b.shape[0]-2)):
-    for j in range(0, (b.shape[1]-2)):
-        aux = b[(i):(i+3),(j):(j+3)]*mascara 
-        #print(np.sum(aux))            
-        b[i+1][j+1]=np.sum(aux)
+#for i in range(0, (b.shape[0]-2)):
+ #   for j in range(0, (b.shape[1]-2)):
+  #      aux = b[(i):(i+3),(j):(j+3)]*mascara 
+        #print(np.sum(aux))
+   #     b[i+1][j+1]=np.sum(aux)
+
+#b = b.reshape(12,12)
+#print(b.shape)
             
         
 
--- taller3/taller3Bordes.py
+++ taller3/taller3Bordes.py
@@ -4,7 +4,7 @@ Autores:
     Leidy Marcela Aldana Burgos - 20151020019
 Programa de manejo de imagenes con Numpy
 Objetivo: Desarrollar un efecto sobre una imagen
-Versión 0. Carece de duplicación de bordes .^^.
+Versión 1. Con duplicación de bordes .^^.
 Realizado haciendo uso de: Editor de Spyder
 """
 
@@ -17,6 +17,12 @@ def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
 
 def recorrer(imagen,mascara):
+    # Duplicando valores para x
+    imagen = np.insert(imagen, 0, imagen[0,:], axis=0)
+    imagen = np.insert(imagen, imagen.shape[0], imagen[(imagen.shape[0]-1),:], axis=0)        
+    # Duplicando valores para y
+    imagen = np.insert(imagen, 0, imagen[:,0], axis=1)
+    imagen = np.insert(imagen, imagen.shape[1], imagen[:,(imagen.shape[1]-1)], axis=1)
     print("Empezando a recorrer: ")    
     for i in range(0, (imagen.shape[0]-2)):
         for j in range(0, (imagen.shape[1]-2)):
@@ -27,13 +33,18 @@ def recorrer(imagen,mascara):
     
 # Leyendo la imagen
 img = mpimg.imread('./image.png')
-# Declaración de máscara
-mascara = np.full((3,3),1/9)          
+# Declaración de máscara original
+# mascara = np.full((3,3),1/9)   
+mascara = np.full((3,3),1) 
 gray = rgb2gray(img)    
 plt.imshow(gray, cmap=plt.get_cmap('gray'), vmin=0, vmax=1)
 plt.show()
 #print(gray.shape[0])
 #print(gray.shape[1])
+
+
+
+
 gray=recorrer(gray,mascara)
 
 print("Después de recorrer")

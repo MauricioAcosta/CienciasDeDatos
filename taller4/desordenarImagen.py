2e483f39cce5875d2558d2ca48f657f7f998e0f5
Andres Mauricio Acosta Pulido <andres.taamap@gmail.com>
2019-04-29 21:16:46 -0500

    :ok_hand:

--- taller4/desordenarImagen.py
+++ taller4/desordenarImagen.py
@@ -3,8 +3,7 @@ Autores:
     Andrés Mauricio Acosta Pulido - 20142020111
     Leidy Marcela Aldana Burgos - 20151020019
 Programa de manejo de imagenes con Numpy
-Objetivo: Desarrollar un efecto sobre una imagen
-Versión 2. Implementación de Efecto .^^.
+Objetivo: Desarrollar un programa que desordene una imagen
 Realizado haciendo uso de: Editor de Spyder
 """
 
@@ -15,41 +14,39 @@ import matplotlib.image as mpimg
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
 
-def comb(matA, matB):
-    return np.array(np.meshgrid(matA, matB)).T.reshape(-1, 3)
+def recorrer(img, desorden):
+    shuffled_image = np.array([])
+    for row in range (0, 4):
+        for aux in range(0, 75):
+            fila = np.array([])
+            for col in range(0,4):
+                r1, c1 = getCords(desorden[row,col])
+                fila = np.hstack((fila, img[r1+aux, c1:c1+75]))
+            if( shuffled_image.shape[0] == 0): shuffled_image = fila
+            else: shuffled_image = np.vstack((shuffled_image, fila))
+    return shuffled_image
 
-def recorrer(imagen, elementos):
-    for i in range(0, 4):
-        for j in range(0, 4):
-            x = (i)*elementos
-            y = (j)*elementos
-            partes=imagen[x:x+elementos,y:y+elementos]
-            
-    return partes
-
-def unirpartes(partes):
-    total=np.vstack((partes[0],partes[1]))
-    for i in range(1,76):
-        total=np.insert(total, total.shape[0], np.hstack((partes[i],partes[i+1])), axis=0)
-        for j in range(2,76):
-            total=np.insert(total, total.shape[1], np.vstack((partes[j],partes[j+1])), axis=1)
-    return total
-    
+def getCords( num ):
+    r = num // 4 
+    c = num %  4 
+    return r*75, c*75
 # Leyendo la imagen
 img = mpimg.imread('./image.png')
 
+aux =np.arange(16)
+np.random.shuffle(aux)
+desorden = aux.reshape((4,4))
+print(desorden)
+print( getCords(15))
 gray = rgb2gray(img)
 gray = gray[:gray.shape[0]-15,:gray.shape[0]-15]
 plt.imshow(gray, cmap=plt.get_cmap('gray'), vmin=0, vmax=1)
 plt.show()
 
 elementos=gray.shape[0]//4
-partes=recorrer(gray, elementos)
 
-desorden=np.arange(16).reshape(4,4)
-np.random.shuffle(desorden)
+partes=recorrer(gray, desorden)
 
-imagendesorden=unirpartes(partes)
-plt.imshow(imagendesorden, cmap=plt.get_cmap('gray'), vmin=0, vmax=1)
+#imagendesorden=unirpartes(partes)
+plt.imshow(partes, cmap=plt.get_cmap('gray'), vmin=0, vmax=1)
 plt.show()
-print(partes[76])

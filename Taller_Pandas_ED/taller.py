a0c1271772a00d654cdd3c4772b50581d9d7c799
LeidyAldana <LeidyMarcelaAldana@gmail.com>
2019-05-13 11:59:19 -0500

    Taller Pandas

---
+++ Taller_Pandas_ED/taller.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Introducción a la Ciencia de Datos
+Estudiantes:
+
+"""
+import numpy as np
+import pandas as pd
+
+docentes=pd.read_csv('DOCENTES_PLANTA_POR_PROGRAMAS_2018-2.csv')
+
+print(docentes.head())

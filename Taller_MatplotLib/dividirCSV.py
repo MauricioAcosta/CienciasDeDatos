898e34f2d87f85010b29364ac5dca0ada9ca01c5
MauricioAcosta <andres.taamap@gmail.com>
2019-06-05 01:10:11 -0500

    update script

--- Taller_MatplotLib/dividirCSV.py
+++ Taller_MatplotLib/dividirCSV.py
@@ -1,3 +1,4 @@
+# DIVIDE el CSV en x CSVs de 500 lineas cada uno 
 import pandas as pd 
 for i,chunk in enumerate(pd.read_csv('Diagn_sticos_emitidos.csv', chunksize=500)):
     chunk.to_csv('Diagn_sticos_emitidos{}.csv'.format(i))

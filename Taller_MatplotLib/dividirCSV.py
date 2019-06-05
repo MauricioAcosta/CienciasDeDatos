66e92524b74ef78d091aee629f330087d55de32e
LeidyAldana <LeidyMarcelaAldana@gmail.com>
2019-06-05 08:58:27 -0500

    se divide C_DIGO__NICO_DE_MEDICAMENTOS_VIGENTES

--- Taller_MatplotLib/dividirCSV.py
+++ Taller_MatplotLib/dividirCSV.py
@@ -1,4 +1,4 @@
 # DIVIDE el CSV en x CSVs de 500 lineas cada uno 
 import pandas as pd 
-for i,chunk in enumerate(pd.read_csv('Diagn_sticos_emitidos.csv', chunksize=500)):
-    chunk.to_csv('Diagn_sticos_emitidos{}.csv'.format(i))
+for i,chunk in enumerate(pd.read_csv('C_DIGO__NICO_DE_MEDICAMENTOS_VIGENTES.csv', chunksize=500)):
+    chunk.to_csv('C_DIGO__NICO_DE_MEDICAMENTOS_VIGENTES{}.csv'.format(i))

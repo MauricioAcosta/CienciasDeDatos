6b51b8b61773f9f1397c106bb9cb2f3ad7c73907
LeidyAldana <LeidyMarcelaAldana@gmail.com>
2019-06-14 10:04:28 -0500

    Estructura de cómo llegará a ser el dataframe

--- appTwitter/PythonTwitter/main.py
+++ appTwitter/PythonTwitter/main.py
@@ -1,13 +1,34 @@
 import twitter
+import pandas as pd
+
 api = twitter.Api(consumer_key='CGMxmM2ubhETj39Q8MVwlC8uJ',
                   consumer_secret='xYswZsui2RTEXHcGmDCZgkkunaezFcjNsu24gPt3FvXXorwzA2',
                   access_token_key='1138117225871814656-j8JK4ak8ahUeOR5Srf81YtNzv8hKXg',
                   access_token_secret='b87o7mB38h7qWczZhVwcUEGRkqjhzP6L3tw07njzzeo8O')
 
-## Tweets that contain the word 'twitter'
-results = api.GetSearch(
-    raw_query="q=twitter%20&result_type=recent&since=2014-07-19&count=100")
-## Tweets that contain the word 'science'
-results2 = api.GetSearch(
-    raw_query="l=en&q=scienc%20&result_type=recent&esince%3A2019-01-01&")
-print(results2)
+def search_tweets():
+    ## Tweets that contain the word 'twitter'
+    results = api.GetSearch(
+            raw_query="q=twitter%20&result_type=recent&since=2014-07-19&count=100")
+    ## Tweets that contain the word 'science'
+    results2 = api.GetSearch(
+            raw_query="l=en&q=science%20&result_type=recent&esince%3A2019-06-14&count=5")
+    return results2 
+
+def fromList_toDf(lista):
+    dfObj = pd.DataFrame(lista)
+    return dfObj
+
+##################################################################################
+#
+# Lógica principal de procesamiento
+# Objetivo: Traer tweets, almacenarlos en Dataframes y analizarlos
+# Autor: Leidy Marcela Aldana 
+#
+##################################################################################
+
+resultados=search_tweets()
+print("resultados[0]=",resultados[0])
+print("resultados[1]=",resultados[1])
+print("resultados[2]=",resultados[2])
+#print(fromList_toDf(resultados))
--- appTwitter/polls/views.py
+++ appTwitter/polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import prueba
 # Create your views here.
 from django.http import HttpResponse
 

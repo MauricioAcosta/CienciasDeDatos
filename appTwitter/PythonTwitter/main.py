d6989b003edacc8409a87b296fa43ecc1e0668ec
LeidyAldana <LeidyMarcelaAldana@gmail.com>
2019-06-16 17:42:02 -0500

    Intentos de convertir lista a dataframe

--- appTwitter/PythonTwitter/main.py
+++ appTwitter/PythonTwitter/main.py
@@ -1,22 +1,28 @@
 import twitter
 import pandas as pd
+import numpy as np
 
 api = twitter.Api(consumer_key='CGMxmM2ubhETj39Q8MVwlC8uJ',
                   consumer_secret='xYswZsui2RTEXHcGmDCZgkkunaezFcjNsu24gPt3FvXXorwzA2',
                   access_token_key='1138117225871814656-j8JK4ak8ahUeOR5Srf81YtNzv8hKXg',
                   access_token_secret='b87o7mB38h7qWczZhVwcUEGRkqjhzP6L3tw07njzzeo8O')
 
+
 def search_tweets():
     ## Tweets that contain the word 'twitter'
-    results = api.GetSearch(
-            raw_query="q=twitter%20&result_type=recent&since=2014-07-19&count=100")
+    #results = api.GetSearch(
+           # raw_query="q=twitter%20&result_type=recent&since=2014-07-19&count=100")
     ## Tweets that contain the word 'science'
     results2 = api.GetSearch(
-            raw_query="l=en&q=science%20&result_type=recent&esince%3A2019-06-14&count=5")
+            raw_query="l=en&q=science%20&result_type=recent&esince%3A2019-06-15&count=10")
+    print(type(results2))
     return results2 
 
 def fromList_toDf(lista):
-    dfObj = pd.DataFrame(lista)
+    dfObj=pd.DataFrame(lista)
+    print("dfObj: ")
+    print(dfObj.head())
+    #print (len(lista))
     return dfObj
 
 ##################################################################################
@@ -28,7 +34,9 @@ def fromList_toDf(lista):
 ##################################################################################
 
 resultados=search_tweets()
-print("resultados[0]=",resultados[0])
-print("resultados[1]=",resultados[1])
-print("resultados[2]=",resultados[2])
-#print(fromList_toDf(resultados))
+#print("resultados[0]=",resultados[0])
+#print("resultados[1]=",resultados[1])
+datafram=fromList_toDf(resultados)
+print("Dataframa")
+print(datafram.columns)
+
